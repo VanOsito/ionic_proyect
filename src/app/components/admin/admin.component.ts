ad427257be8176b098fee860f7506ec44408be4e
Vania <van.troncosom@duocuc.cl>
2023-11-26 16:56:07 -0300

    agregado vista

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { DataBaseService } from 'src/app/services/data-base.service';
+
+import { Asistencia } from 'src/app/model/asistencia';
+import { IonicModule, ViewWillEnter } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
+  imports: [IonicModule, CommonModule, FormsModule],
+  standalone: true,
 })
 export class AdminComponent  implements OnInit {
-  esAdministrador: boolean = false;
 
   constructor(private authService: AuthService) {
-    if (this.authService.esAdministrador) {
-    console.log('¡Eres un administrador!');
-  } else {
-    console.log('No eres un administrador.');
-  }
+    
 }
 
 
--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -9,19 +9,41 @@ import { ForoComponent } from 'src/app/components/foro/foro.component';
 import { MisdatosComponent } from 'src/app/components/misdatos/misdatos.component';
 import { DataBaseService } from 'src/app/services/data-base.service';
 import { APIClientService } from 'src/app/services/apiclient.service';
+import { AdminComponent } from 'src/app/components/admin/admin.component';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule,
-    QrComponent, MiclaseComponent, ForoComponent, MisdatosComponent,
+  imports: [IonicModule, 
+    CommonModule,
+    FormsModule,
+    QrComponent, 
+    MiclaseComponent, 
+    ForoComponent, 
+    MisdatosComponent, 
+    AdminComponent
   ],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('500ms ease-out', style({ transform: 'translateX(0%)' }))
+      ]),
+      transition(':leave', [
+        animate('500ms ease-out', style({ transform: 'translateX(100%)' }))
+      ])
+    ])
+  ]
 })
 export class InicioPage implements OnInit {
 
   componente_actual = 'qr';
+  admin_: boolean = false;
+
+  
 
   constructor(
     private authService: AuthService, 
@@ -29,18 +51,26 @@ export class InicioPage implements OnInit {
     private api: APIClientService) { }
 
   ngOnInit() {
-    this.componente_actual = 'qr';
+    this.authService.leerUsuarioAutenticado().then((userData) => {
+      this.admin_ = userData?.nombre === 'admin';
+    });
+    this.componente_actual = '';
     this.bd.datosQR.next('');
+    
   }
 
   cambiarComponente(nombreComponente: string) {
     this.componente_actual = nombreComponente;
-    if (this.componente_actual === 'foro') this.api.cargarPublicaciones();
-    if (this.componente_actual === 'misdatos') this.authService.leerUsuarioAutenticado();
+    if (this.componente_actual === 'foro' && !this.admin_) {
+    } else if (this.componente_actual === 'foro') {
+      this.api.cargarPublicaciones();
+    }
   }
+
 
   cerrarSesion() {
     this.authService.logout();
   }
+
 
 }
